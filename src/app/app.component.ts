2d61cd945afa9afe5eed9110a6f62cef2c1e81a0
N1-PGIRANDA\Paolo Giranda <p.giranda@be-tse.it>
2019-01-22 17:27:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
 import {ProductsPage} from "../pages/products/products";
 
 @Component({
@@ -36,6 +36,7 @@ export class MyApp {
       this.statusBar.styleDefault();
 
       this.splashScreen.hide();
+
     });
   }
 
